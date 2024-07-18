cf1b383db5885d065381c82ca96b82072bb17d4a
IshwarTandi2 <ishwartandi26@gmail.com>
2024-07-18 23:42:25 +0530

    Problem 6

---
+++ SumofNatural.java
@@ -0,0 +1,17 @@
+package RECURSION;
+
+public class SumofNatural {
+
+
+    public static int Sum(int n){
+          if(n==1){
+            return 1;
+          }
+          return n + Sum(n-1);
+    }
+    public static void main(String args[]){
+      System.out.print(Sum(5));
+    }
+    
+}
+
