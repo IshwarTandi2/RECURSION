38b41f60127bf087e7adec30984a46958dbee71e
IshwarTandi2 <ishwartandi26@gmail.com>
2025-01-27 18:37:50 +0530

    Find first & last ocuurence of element in an array

--- Rec6.java
+++ Rec6.java
@@ -9,9 +9,19 @@ public class Rec6 {
         }
         return firstOccurence(arr, i+1, key);
     }
+    public static int lastOccurence(int arr[],int i , int key){
+        if(i==arr.length){
+            return -1;
+        }
+        int isFound = lastOccurence(arr, i+1, key);
+        if(isFound == -1 && arr[i] == key){
+            return i;
+        }
+        return isFound;
+    }
     public static void main (String args[]){
-        int arr[] = {1,2,4,5,3};
-        System.out.print(firstOccurence(arr, 0, 3));
+        int arr[] = {1,2,4,5,3,4};
+        System.out.print(lastOccurence(arr, 0, 4));
 
     }
 }
