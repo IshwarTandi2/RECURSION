c00957e22b1ddf2d2ad81408f81430659b5fda02
IshwarTandi2 <ishwartandi26@gmail.com>
2025-01-30 18:53:36 +0530

    Print Binary Strings without consecutive ones

---
+++ Rec11.java
@@ -0,0 +1,28 @@
+public class Rec11 {
+    
+    public static void printBinStrings(int n , int lastplace , String newstr){
+        if(n==0){
+            System.out.println(newstr);
+            return;
+        }
+
+       /* if(lastplace == 0){
+            printBinStrings(n, 0, newstr+="0");
+            printBinStrings(n, 1, newstr+="1");
+        }
+        else {
+            // condition - when -> if(lastplace == 1)
+            printBinStrings(n, 0, newstr+="0"); 
+            }  */
+         
+            printBinStrings(n-1, 0, newstr+"0");
+            if(lastplace == 0){
+             printBinStrings(n-1, 1, newstr+"1"); 
+
+       }   
+    }
+
+    public static void main (String args[]){
+        printBinStrings(3, 0, "");
+    }
+}
